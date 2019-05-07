2c7734459334b51d0f93d3ce67af75114d805492
keniseli <ken.iseli@gmail.com>
2019-05-07 19:14:02 +0200

    Implemented draft for alarm state machine.

---
+++ Arduino/build-monitor/build-monitor.ino
@@ -0,0 +1,64 @@
+#include <ChainableLED.h>
+
+#define NUM_LEDS 1
+
+int clk_pin = 9; // D4
+int data_pin = 10; // D5
+ChainableLED leds(clk_pin, data_pin, NUM_LEDS);
+
+int button_pin = A0;
+int button_value = LOW;
+
+int buzzer_pin = 5; // D2
+
+int state = 0;
+// 0 = not in alarm state
+// 1 = in alarm state
+
+int alarm_state = 0;
+// 0 = green, everything okay
+// 1 = red, alarm
+// 2 = blue, alarm
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(clk_pin, OUTPUT);
+  pinMode(data_pin, OUTPUT);
+  pinMode(button_pin, INPUT);
+  pinMode(buzzer_pin, OUTPUT);
+  digitalWrite(buzzer_pin, LOW);
+}
+
+void loop() {
+  Serial.begin(115200);
+  button_value = digitalRead(button_pin);
+
+  if (state == 0 && button_value == HIGH) {
+    Serial.println("state 0, button triggers state 1 for testing");
+    state = 1;
+    
+  } else if(state == 1 && button_value == HIGH) {
+    Serial.println("state 1, button triggers state 0");
+    state = 0;
+    alarm_state = 0;
+    
+  } else if (state == 1 && button_value == LOW) {
+    if (alarm_state != 1) {
+      alarm_state = 1;
+    } else {
+      alarm_state = 2;
+    }
+  }
+
+  if (alarm_state == 0) {
+    leds.setColorRGB(0, 0, 255, 0);
+    digitalWrite(buzzer_pin, LOW);
+  } else if (alarm_state == 1) {
+    leds.setColorRGB(0, 255, 0, 0);
+    digitalWrite(buzzer_pin, HIGH);
+  } else if (alarm_state == 2) {
+    leds.setColorRGB(0, 0, 255, 255);
+    digitalWrite(buzzer_pin, LOW);
+  }
+  delay(100);
+}
